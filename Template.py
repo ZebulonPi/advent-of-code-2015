aa252f871aeb3b8352a4c975609575adf9e07ba7
Jason Greenwood <zebulonpi@gmail.com>
2024-09-04 09:32:22 -0400

    Added template file and Day 1 text files

--- Template.py
+++ Template.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+import re
+
+
+def main():
+    with open('C:\\Users\\zebul\\AdventOfCode\\2015\\Dat1InputTest.txt') as f:
+        main_raw_input = [line.rstrip('\n') for line in f]
+
+    # if you need the input broken up into constituent parts:
+    # converted_input = main_raw_input[0].split(',')
+    # converted input = [int(i) for i in main_raw_input] # for things like ints
+
+    for row in main_raw_input:
+        print(row)
+
+        # if you should need to break it into single units of the row, use this:
+        # for single_unit in row:
+        #     print(single_unit)
+
+
+if __name__ == '__main__':
+    main()
